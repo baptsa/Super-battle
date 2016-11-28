47f9b5488ece90a70409202bbc89ee9890131b9a
Baptiste Saint-Aubin <baptiste.saintaubin@gmail.com>
2016-11-28 17:00:42 +0100

    model OK

--- app/models/insta_user.rb
+++ app/models/insta_user.rb
@@ -1,7 +1,7 @@
 class InstaUser < ApplicationRecord
   # belongs_to :user
-  has_many :fights_as_competitor, class_name: "Fight", foreign_key: :competitor_id
-  has_many :fights_as_challenger, class_name: "Fight", foreign_key: :challenger_id
+  has_many :fights_as_competitor, class_name: "Fight", foreign_key: :opponent_id, dependent: :destroy
+  has_many :fights_as_challenger, class_name: "Fight", foreign_key: :challenger_id, dependent: :destroy
 
   def fights
     fights_as_challenger + fights_as_competitor
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+InstaUser.destroy_all
+
+jo = User.new(email:"jomourot@supper-battle.com", password:"azerty")
+jo.save!
+
+paal = User.new(email:"paal@supper-battle.com", password:"azerty")
+paal.save!
+
+zguy = User.new(email:"zguy@supper-battle.com", password:"azerty")
+zguy.save!
+
+
+beyonce = InstaUser.new(profile_picture: "url", first_name:"Beyoncé", last_name:"Beyoncé", bio:"coucou c'est beyoncé", media:8, follow:10, followed_by:30, user_id:jo.id )
+beyonce.save!
+
+
+shakira = InstaUser.new(profile_picture: "url", first_name:"shakira", last_name:"shakira", bio:"coucou c'est shakira", media:8, follow:10, followed_by:30, user_id:paal.id )
+shakira.save!
+
+
