39de0cf1f34987a85b0955b2680bffb70fbeb99b
Joseph Mourot <mourot.joseph@gmail.com>
2016-11-29 17:17:23 +0100

    worked on the API

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @popular = Instagram.user_search("fantasticmisterzguy")
   end
 end
