46bf12ea3000599baceaedc25c1975ae7cb6fcf1
Ghislain Chavanne <gh.chavanne@gmail.com>
2016-11-29 16:07:59 +0100

    added video and changed validations

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -3,7 +3,7 @@ class Fight < ApplicationRecord
   belongs_to :challenger, :class_name => 'InstaUser', :foreign_key => :challenger_id
   belongs_to :opponent, :class_name => 'InstaUser', :foreign_key => :opponent_id
   has_many :insta_users
-  validates :challenger_id, uniqueness: true, allow_blank: false
-  validates :opponent_id, uniqueness: true, allow_blank: false
+  validates :challenger_id, presence: true, allow_blank: false
+  validates :opponent_id, presence: true, allow_blank: false
 
 end
