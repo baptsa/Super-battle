1cedea4f1e9ff639693c479263dd7ef4b63d4587
Paal Ringstad <paalringstad@gmail.com>
2016-11-30 16:04:55 +0100

    algo cleaned

--- app/algo/score_algorithm.rb
+++ app/algo/score_algorithm.rb
@@ -2,29 +2,80 @@ def followers_score(insta_user)
   score = 0
   followed_by = insta_user.followed_by
   if followed_by < 100
-    score += 0
-  elsif followed_by >= 100 && followed_by < 200
-    score += 2
-  elsif followed_by >= 200 && followed_by < 500
-    score += 5
-  elsif followed_by >= 500 && followed_by < 1000
-    score += 9
-  elsif followed_by >= 1000 && followed_by < 2000
-    score += 13
-  elsif followed_by >= 2000 && followed_by < 4000
-    score += 17
-  elsif followed_by >= 4000 && followed_by < 8000
-    score += 21
-  elsif followed_by >= 8000 && followed_by < 12000
-    score += 25
-  elsif followed_by >= 12000 && followed_by < 20000
-    score += 30
-  elsif followed_by >= 20000 && followed_by < 50000
-    score += 35
-  elsif followed_by >= 50000
-    score += 40
+    score = 0
+  elsif followed_by < 200
+    score = 2
+  elsif followed_by < 500
+    score = 5
+  elsif followed_by < 1000
+    score = 9
+  elsif followed_by < 2000
+    score = 13
+  elsif followed_by < 4000
+    score = 17
+  elsif followed_by < 8000
+    score = 21
+  elsif followed_by < 12000
+    score = 25
+  elsif followed_by < 20000
+    score = 30
+  elsif followed_by < 50000
+    score = 35
+  else
+    score = 42
   end
 end
+
+def score_media(insta_user)
+  media = insta_user.media.to_i
+  interval1 = 10
+  interval2 = 30
+  interval3 = 50
+  interval4 = 70
+  interval5 = 100
+  interval6 = 150
+  interval7 = 200
+  interval8 = 300
+  interval9 = 500
+  interval10 = 1000
+  media_score = 0
+  points = 2
+
+  if media < interval1
+    media_score = 0
+  elsif media < interval2
+    media_score = points * 1
+  elsif media < interval3
+    media_score = points * 2
+  elsif media < interval4
+    media_score = points * 3
+  elsif media < interval5
+    media_score = points * 4
+  elsif media < interval6
+    media_score = points * 5
+  elsif media < interval7
+    media_score = points * 6
+  elsif media < interval8
+    media_score = points * 7
+  elsif media < interval9
+    media_score = points * 8
+  elsif media < interval10
+    media_score = points * 9
+  else
+    media_score = points * 10
+  end
+   # for i in 1..9
+  #   return_media(i)
+  # end
+
+  # def return_media(number)
+  #   elseif media < "media#{number}".to_sym
+  #     media = points * "number".to_i
+  #   end
+  # end
+end
+
+
 def engagement_score(insta_user)
   score = 0
   engagement = insta_user.engagement / insta_user.followed_by
@@ -85,64 +136,6 @@ def engagement_score(insta_user)
       score += 40
     end
   end
-
-def score_media(insta_user)
-  media = insta_user.media.to_i
-  interval1 = 10
-  interval2 = 30
-  interval3 = 50
-  interval4 = 70
-  interval5 = 100
-  interval6 = 150
-  interval7 = 200
-  interval8 = 300
-  interval9 = 500
-  interval10 = 1000
-  media_score = 0
-  points = 2
-
-  if media < interval1
-    media_score = points * 1
-    return media_score
-  elsif media < interval2
-    media_score = points * 2
-    return media_score
-  elsif media < interval3
-    media_score = points * 3
-    return media_score
-  elsif media < interval4
-    media_score = points * 4
-    return media_score
-  elsif media < interval5
-    media_score = points * 5
-    return media_score
-  elsif media < interval6
-    media_score = points * 6
-    return media_score
-  elsif media < interval7
-    media_score = points * 7
-    return media_score
-  elsif media < interval8
-    media_score = points * 8
-    return media_score
-  elsif media < interval9
-    media_score = points * 9
-    return media_score
-  else
-    media_score = points * 10
-    return media_score
-  end
-   # for i in 1..9
-  #   return_media(i)
-  # end
-
-  # def return_media(number)
-  #   elseif media < "media#{number}".to_sym
-  #     media = points * "number".to_i
-  #     return media
-  #   end
-  # end
 end
-
 
 
