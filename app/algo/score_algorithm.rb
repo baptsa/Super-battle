c70eea2d6e2b0b33e0cfc3dff20d79f9dc409967
Paal Ringstad <paalringstad@gmail.com>
2016-11-30 14:35:12 +0100

    adding method media algo

---
+++ app/algo/score_algorithm.rb
@@ -0,0 +1,41 @@
+media1 = 10
+media2 = 30
+media3 = 50
+media4 = 70
+media5 = 100
+media6 = 150
+media7 = 200
+media8 = 300
+media9 = 500
+media10 = 1000
+
+def score_score_media(user)
+  media = user.media
+  points = 2
+  score_media = 0
+  if media < media1
+    return media = points * 1
+  elsif media < media2
+    return media = points * 2
+  elsif media < media3
+    return media = points * 3
+  elsif media < media4
+    return media = points * 4
+  elsif media < media5
+    return media = points * 5
+  elsif media < media6
+    return media = points * 6
+  elsif media < media7
+    return media = points * 7
+  elsif media < media8
+    return media = points * 8
+  elsif media < media9
+    return media = points * 9
+  else
+    return media = pf10 * 10
+  end
+end
+
+score_followers(paal)
+
+
