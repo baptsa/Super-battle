8e8db090e74e68a38e9a142a4e25aca9e0184237
Joseph Mourot <mourot.joseph@gmail.com>
2016-11-30 19:18:02 +0100

    made some adjustments with Gab

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,9 +1,6 @@
    require "instagram"
 
     Instagram.configure do |config|
-
-    config.client_id = "8121a2b2f08e4faa931c139cddc448a3"
-
-    config.access_token = "2024147607.8121a2b.aebb392adc3f4ab98efa6e4025caaa40"
-
+      config.client_id =  ENV["IG_ID"]
+      config.client_secret = ENV["IG_SECRET"]
     end
