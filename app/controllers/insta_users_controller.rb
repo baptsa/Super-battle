d2a8ebf461efae99b6b0327e56d277f2406a0ed5
Baptiste Saint-Aubin <baptiste.saintaubin@gmail.com>
2016-12-07 17:56:18 +0100

    working on autocomplete

--- app/controllers/insta_users_controller.rb
+++ app/controllers/insta_users_controller.rb
@@ -1,4 +1,14 @@
 class InstaUsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
+
+  def index
+    @insta_users = InstaUser.where("username iLIKE ?", "%#{params[:username]}%")
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
   def new
     @insta_user = InstaUser.new()
   end
@@ -6,7 +16,7 @@ class InstaUsersController < ApplicationController
   def create
   end
 
-   def autocomplete
+  def autocomplete
     client = Instagram.client(:access_token => "1396122422.8121a2b.120c5c5852944ba1a82a69f4da119392")
     value = client.user_search(params[:query])
     @username = value[0]["username"]
