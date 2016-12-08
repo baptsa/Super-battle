0d7529bace01e885d99282d52938a6b9afa0d7a6
Joseph Mourot <mourot.joseph@gmail.com>
2016-12-08 17:36:05 +0100

    changed omniauth stuff

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
 
   config.omniauth :instagram, ENV["IG_ID"], ENV["IG_SECRET"],
 
-    scope: 'public_content basic follower_list',
+    scope: 'basic',
     image_size: 'square',  # 50x50, guaranteed ratio
     secure_image_url: true
 
